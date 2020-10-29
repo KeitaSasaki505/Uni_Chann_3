a589e3ef2055567884a427ce8e2222042e04f511
KeitaSasaki505 <cutba505@gmail.com>
2020-10-30 00:35:58 +0900

    ビュー実装2

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [ ]
+  before_action :set_comment, only: [ ]
+
+  def create
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_134743) do
+ActiveRecord::Schema.define(version: 2020_10_29_152735) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2020_10_29_134743) do
     t.string "phone", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id_id"], name: "index_events_on_user_id_id"
   end
 
