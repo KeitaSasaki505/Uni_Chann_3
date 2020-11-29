eb3bc408b4f53b8340868e74c4f37629111dbd93
KeitaSasaki505 <cutba505@gmail.com>
2020-11-29 20:02:16 +0900

    調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "Uni_Chann_3"
-set :repo_url, "git@example.com:me/my_repo.git"
+
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:KeitaSasaki505/Uni_Chann_3.git'
 
