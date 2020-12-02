595b8f0b6ddf98a6112479c8ddeddcba1b65508c
KeitaSasaki505 <cutba505@gmail.com>
2020-12-02 22:55:30 +0900

    調整

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,46 @@
-Geocoder.configure(
+# if Rails.env.production? then
+# Geocoder.configure(
+#   # Geocoding options
+#   timeout: 5,                 # geocoding service timeout (secs)
+#   lookup: :google,         # name of geocoding service (symbol)
+#   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
+#   # language: :en,              # ISO-639 language code
+#   use_https: true,           # use HTTPS for lookup requests? (if supported)
+#   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
+#   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
+#   api_key: Settings.google.geocorder_key,              # API key for geocoding service
+#   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
+#   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
+
+#   # Exceptions that should not be rescued by default
+#   # (if you want to implement custom error handling);
+#   # supports SocketError and Timeout::Error
+#   # always_raise: [],
+
+#   # Calculation options
+#   units: :km,                 # :km for kilometers or :mi for miles
+#   # distances: :linear          # :spherical or :linear
+#   always_raise: [
+#     Geocoder::OverQueryLimitError,
+#     Geocoder::RequestDenied,
+#     Geocoder::InvalidRequest,
+#     Geocoder::InvalidApiKey
+#   ],
+# )
+# else
+#   Geocoder.configure(
+#     lookup: :google,
+#     always_raise: [
+#       Geocoder::OverQueryLimitError,
+#       Geocoder::RequestDenied,
+#       Geocoder::InvalidRequest,
+#       Geocoder::InvalidApiKey
+#     ],
+#     api_key: Settings.google.map_key
+#   )
+#   end
+  
+  Geocoder.configure(
   # Geocoding options
   timeout: 5,                 # geocoding service timeout (secs)
   lookup: :google,         # name of geocoding service (symbol)
