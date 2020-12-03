1b82a80849448175bf439234cb8b95d3d6dd5310
KeitaSasaki505 <cutba505@gmail.com>
2020-12-03 12:16:08 +0900

    不要項目削除

--- app/models/join.rb
+++ app/models/join.rb
@@ -5,7 +5,7 @@ class Join < ApplicationRecord
 
   with_options presence: true do
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, {format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+    validates :email, format: { with: VALID_EMAIL_REGEX }
     VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
     validates :phone, format: { with: VALID_PHONE_REGEX }
     validates :name_kanji, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}\s|[ー－]|[一-龠々])+\z/, message: 'は全角で入力してください' }
