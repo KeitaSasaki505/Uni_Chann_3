25c758e83f3cfdfc528ab338c43c0e64aeb947fe
KeitaSasaki505 <cutba505@gmail.com>
2020-12-22 11:55:06 +0900

    調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "3.14.1"
 
-set :application, "Uni_Chann_3"
+set :application, 'Uni_Chann_3'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url, 'git@github.com:KeitaSasaki505/Uni_Chann_3.git'
