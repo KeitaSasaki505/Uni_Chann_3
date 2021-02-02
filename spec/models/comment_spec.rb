3e294c1523e34d4f111d0e7812ac569d4f5f70ff
KeitaSasaki505 <cutba505@gmail.com>
2021-02-02 19:41:20 +0900

    追記

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,6 +5,9 @@ describe Comment do
     @event = FactoryBot.build(:event)
     @event.images = fixture_file_upload('/files/test_image.png')
     @comment = FactoryBot.build(:comment, user_id: @user.id, event_id: @event.id)
+    # 画像アップロードのテスト結果を確認する
+    # 画像に関連する項目の調整
+    # images can't be blank → 正常系
   end
 
   describe 'コメント投稿機能' do
